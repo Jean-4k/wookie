6a83ba7a1d1a9e0bee181d97b6a195ba70908297
Jean-4k <45103062+Jean-4k@users.noreply.github.com>
2019-09-05 15:11:02 +0200

    Indentation of config.lisp.

--- config.lisp
+++ config.lisp
@@ -5,23 +5,36 @@
            #:*hide-version*
            #:*enabled-plugins*
            #:*tmp-file-store*))
+
+
+
 (annot:enable-annot-syntax)
 (in-package :wookie-config)
 
+
+
 (defvar *debug-on-error* nil
   "If T, will not catch errors but instead allow them to go to the debugger.")
+
+
 
 (defvar *max-body-size* (* 1024 1024 2)
   "The max HTTP body size in bytes that Wookie will store for each request,
    assuming the request is set to store the body (see request-store-body).")
 
+
+
 (defvar *hide-version* nil
   "Boolean specifying whether or not to hide the Wookie version in the Server
    header.")
 
+
+
 @export
 (defvar *enabled-plugins* '(:get :post :multipart :http-var :cookie :directory-router)
   "A list of (keyword) names of enabled plugins.")
+
+
 
 (defvar *tmp-file-store* (asdf:system-relative-pathname :wookie #p"upload-tmp/")
   "Stores the path to where uploads/temporary files go.")
